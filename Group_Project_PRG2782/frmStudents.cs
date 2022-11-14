cd1e5adb92b472a26c04fb8ee4691ccc686169d3
GhostlyTK <tokelojr@gmail.com>
2022-11-14 10:13:18 +0200

    DB restructure
    Restructured to Database Db Update_2

---
+++ Group_Project_PRG2782/frmStudents.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_Project_PRG2782
+{
+    public partial class frmStudents : Form
+    {
+        public string sqlString;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        DataHandler data = new DataHandler();
+
+        public frmStudents()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void GetStudents()
+        {
+            try
+            {
+                data.Sqlcon.Open();
+                sqlString = @"Select * from Students";
+
+
+            }
+            catch (Exception t)
+            {
+                MessageBox.Show(t.Message);
+                throw;
+            }
+        }
+    }
+}
